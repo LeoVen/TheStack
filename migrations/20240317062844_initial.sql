f2107fd6276c62bd958f1acd57099a59177b6c71
Leonardo Vencovsky <lvenk26@gmail.com>
2024-03-30 02:03:54 +0100

    New database schemas

--- migrations/20240317062844_initial.sql
+++ migrations/20240317062844_initial.sql
@@ -1,8 +1,19 @@
 -- Add migration script here
-CREATE TABLE spell (
-  id bigserial primary key,
-  name VARCHAR,
-  damage INT not null,
-  created_at TIMESTAMPTZ NOT NULL default now(),
-  updated_at TIMESTAMPTZ NOT NULL default now()
+create table if not exists coupon_set (
+    "id" bigserial,
+
+    "name" varchar,
+    "created_at" timestamptz not null default now(),
+
+    primary key("id")
+);
+
+create table if not exists coupon (
+    "id" uuid default gen_random_uuid(),
+    "set_id" bigserial,
+
+    "used" boolean default false,
+
+    primary key("id"),
+    constraint fk_coupon_set foreign key("set_id") references coupon_set("id")
 );
--- migrations/20240317062906_data.sql
+++ migrations/20240317062906_data.sql
@@ -1,10 +1,14 @@
 -- Add migration script here
-INSERT INTO spell (id, name, damage) VALUES
-(1, 'Fireball', 30),
-(2, 'Ice Shard', 25),
-(3, 'Thunderbolt', 35),
-(4, 'Earthquake', 40),
-(5, 'Healing Light', -25),
-(6, 'Arcane Missile', 20),
-(7, 'Poison Cloud', 15),
-(8, 'Chain Lightning', 45);
+do
+$proc$
+declare
+    c bigint;
+begin
+    select count(*) from coupon_set into c;
+    with
+        set_series as (select * from generate_series(c, c+24) as n),
+        sets as (insert into coupon_set ("name") select 'Coupon Set ' || (ss.n + 1) from set_series ss returning id),
+        coupon_series as (select * from generate_series(1, 10))
+    insert into coupon ("set_id") select cs.id from sets as cs left join coupon_series on 1=1;
+end
+$proc$;
